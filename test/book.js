925a7901847630112a65a32717d6cc191ac77364
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-07-03 21:42:17 +0100

    added test for post path

--- test/book.js
+++ test/book.js
@@ -1,6 +1,6 @@
 // During the test the env variable is set to test
 
-process.send.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
 let mongoose = require("mongoose");
 let Book = require('../controllers/models/book');
@@ -35,5 +35,29 @@ describe('Books', () => {
             });
       });
   });
+
+/*
+* Test the /POST route
+*/   
+
+  describe('/POST book', ()=> {
+        it('it should not POST a book without pages field', (done) =>{
+            let book ={
+                title : "The Lord of the Rings",
+                author:  "J.R.R. Tolkien",
+                year : 1954
+            }
+            chai.request(server)
+                .post('/book')
+                .send(book)
+                .end((err, res) =>{
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    res.body.should.have.property('errors')
+                    res.body.errors.should.have.property('pages')
+                    res.body.errors.pages.should.have.property('kind')
+                })
+        })
+  })
 
 });
